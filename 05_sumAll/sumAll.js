4aaa3260c6e74c5fed2b93f2bde91694e93579c2
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-10-20 23:00:12 +0330

    Handle descending range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
 const sumAll = function (start, end) {
     let sum = 0;
-    for (let i = start; i <= end ; i++) {
+    for (
+        let i = (start < end ? start : end);
+        i <= (start < end ? end : start);
+        i++
+    ) {
         sum += i;
     }
     return sum;
