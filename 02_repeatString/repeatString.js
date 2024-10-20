1f30a170c5d9a9e4285bfc9087f406fc1419e056
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-10-20 22:10:34 +0330

    Add functionality to repeat string several times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function (string, count) {
 
+    let newString = "";
+    for (let i = 0; i < count; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
