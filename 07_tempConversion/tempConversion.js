654a0c8a2d3c92912cd59c56547c21edabc15008
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-10-21 17:46:46 +0330

    Add functionality to convert cel to far

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,17 @@
-const convertToCelsius = function(temp) {
-  const tempInCelsius = +((temp - 32) * 5 / 9);
-  const roundedTemp = +(tempInCelsius.toFixed(1));
-  return roundedTemp;
+const convertToCelsius = function (temp) {
+    const tempInCelsius = +((temp - 32) * 5 / 9);
+    const roundedTemp = +(tempInCelsius.toFixed(1));
+    return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    const tempInFahrenheit = +((temp * 9 / 5) + 32)
+    const roundedTemp = +(tempInFahrenheit.toFixed(1));
+    return roundedTemp;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
