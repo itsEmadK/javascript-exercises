5881e8e527dd25fd68548786116a8733cdf7e409
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-10-21 17:43:47 +0330

    Add functionality to convert faren to cel

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,7 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const tempInCelsius = +((temp - 32) * 5 / 9);
+  const roundedTemp = +(tempInCelsius.toFixed(1));
+  return roundedTemp;
 };
 
 const convertToFahrenheit = function() {
